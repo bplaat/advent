58de61ec19bd413c32324e3040fd9847d0520544
Bastiaan van der Plaat <bastiaan.v.d.plaat@gmail.com>
2024-12-05 09:45:37 +0100

    Add day2 part2

---
+++ day2/part2.py
@@ -0,0 +1,36 @@
+def check(numbers):
+    for i in range(len(numbers) - 1):
+        diff = abs(numbers[i] - numbers[i + 1])
+        if numbers[i + 1] >= numbers[i] or diff < 1 or diff > 3:
+            break
+    else:
+        return True
+
+    for i in range(len(numbers) - 1):
+        diff = abs(numbers[i] - numbers[i + 1])
+        if numbers[i + 1] <= numbers[i] or diff < 1 or diff > 3:
+            break
+    else:
+        return True
+
+    return False
+
+with open('input.txt') as f:
+    data = f.readlines()
+
+    are_safe = 0
+    for line in data:
+        numbers = [int(part.strip()) for part in line.split(' ')]
+
+        safe = check(numbers)
+        if not safe:
+            for i in range(len(numbers)):
+                copy = numbers.copy()
+                del copy[i]
+                if check(copy):
+                    safe = True
+                    break
+
+        if safe:
+            are_safe += 1
+    print(are_safe)
